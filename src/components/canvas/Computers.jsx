a2ef25696ad880f3c20642d3976a2b99875bb5fc
ShawnTseng <shawntseng40@gmail.com>
2023-07-28 14:42:52 +0800

    feat: add loader for 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -34,8 +34,10 @@ const ComputersCanvas = () => {
     <Canvas frameloop='demend' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        <Computers />
       </Suspense>
-      <Computers />
+
+      <Preload all />
     </Canvas>
   )
 }
